fa959beb1ec7517a52a99ae2cffc56f5291553cf
eritho91 <erik.thorell@iths.se>
2026-04-08 10:27:43 +0200

    init commit

---
+++ src/main/java/se/iths/erikthorell/webshopprojekt/WebshopProjektApplication.java
@@ -0,0 +1,13 @@
+package se.iths.erikthorell.webshopprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopProjektApplication.class, args);
+    }
+
+}
