e4ba1d91d8a329be50773859463ec75a74d24604
eritho91 <erik.thorell@iths.se>
2026-04-08 12:12:48 +0200

    fixat app props för test-klass

--- src/test/java/se/iths/erikthorell/webshopprojekt/WebshopProjektApplicationTests.java
+++ src/test/java/se/iths/erikthorell/webshopprojekt/WebshopProjektApplicationTests.java
@@ -1,13 +1,13 @@
-//package se.iths.erikthorell.webshopprojekt;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class WebshopProjektApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package se.iths.erikthorell.webshopprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
