b9640297ebedfe9b61ff809fd2e9de59c48d16bb
OP-Devloop <oscar.pettersson@iths.se>
2026-04-14 15:06:39 +0200

    add AdminProductController and enhance ProductController with product management methods

--- src/main/java/se/iths/erikthorell/webshopprojekt/controller/ProductController.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/controller/ProductController.java
@@ -1,7 +1,59 @@
 package se.iths.erikthorell.webshopprojekt.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import se.iths.erikthorell.webshopprojekt.model.Category;
+import se.iths.erikthorell.webshopprojekt.model.Product;
+import se.iths.erikthorell.webshopprojekt.service.ProductService;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Controller
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    public Product getProductByName(String name) {
+        return productService.getProductByName(name);
+    }
+
+    public Product getProductByCategory(Category category) {
+        return productService.getProductByCategory(category);
+    }
+
+    public Product getProductByPrice(BigDecimal price) {
+        return productService.getProductByPrice(price);
+    }
+
+    @PostMapping
+    public Product createProduct(@Valid @RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("/{id}")
+    public Product updateProduct(@Valid @RequestBody Long id, Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PutMapping("/{id}")
+    public void deleteProduct(@Valid @RequestBody Long id) {
+        productService.deleteProduct(id);
+    }
 }
