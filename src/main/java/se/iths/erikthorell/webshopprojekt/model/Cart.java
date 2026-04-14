e0c44fcfeec4dd3719181dbbd45da2e0e2bf5eca
klasen98 <klasen98@live.se>
2026-04-14 14:51:52 +0200

    fixat cart, och ändrat från bigdecimal till double

--- src/main/java/se/iths/erikthorell/webshopprojekt/model/Cart.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/model/Cart.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Cart {
 
- 
+
     // Lista för kundvagnen
     private List<CartItem> items = new ArrayList<>();
 
--- src/main/java/se/iths/erikthorell/webshopprojekt/model/Product.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/model/Product.java
@@ -1,11 +1,5 @@
 package se.iths.erikthorell.webshopprojekt.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.Getter;
-import lombok.Setter;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -14,13 +8,9 @@ import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-
 @Getter
 @Setter
 @Entity
-@Getter
-@Setter
 public class Product {
 
     @Id
@@ -36,7 +26,7 @@ public class Product {
     private Category category;
 
     @Positive
-    private BigDecimal price;
+    private double price;
 
     @Min(0)
     private int stock;
@@ -49,7 +39,7 @@ public class Product {
     public Product() {
     }
 
-    public Product(String name, Category category, BigDecimal price, int stock, String description, String imageUrl) {
+    public Product(String name, Category category, double price, int stock, String description, String imageUrl) {
         this.name = name;
         this.category = category;
         this.price = price;
