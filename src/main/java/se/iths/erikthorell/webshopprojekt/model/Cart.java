7a36a8c548492aa6762a3d7c3d179f30e0634c3c
klasen98 <klasen98@live.se>
2026-04-14 11:40:17 +0200

    Cart och CartItem klasser klara, la även till getter och setter @ i product klassen för att kunna använda de

---
+++ src/main/java/se/iths/erikthorell/webshopprojekt/model/Cart.java
@@ -0,0 +1,46 @@
+package se.iths.erikthorell.webshopprojekt.model;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+ 
+    // Lista för kundvagnen
+    private List<CartItem> items = new ArrayList<>();
+
+
+    public void addProduct(Product product) {
+        for (CartItem item : items) {
+
+            //jämför ID på produkten i vagnen med produkt ID vi vill lägga till
+            if (item.getProduct().getId().equals(product.getId())) {
+                // om varan finns +1
+                item.setQuantity(item.getQuantity() + 1);
+                return;
+            }
+        }
+        // finns inte varan läggs +1 ny produkt till
+        items.add(new CartItem(product, 1));
+
+    }
+
+    // returnera hela listan med varor så vi kan visa dem på HTML sidan
+    public List<CartItem> getItems() {
+        return items;
+    }
+
+    // tömmer kundvagnen när vi handlat klart
+    public void clear() {
+        items.clear();
+    }
+
+    // räknar ut totala summan för hela kundvagnen
+    public double getTotalCartPrice() {
+        return items
+                .stream() // kollar igenom kundvagnen
+                .mapToDouble(CartItem::getTotalPrice) // omvandlar varorna till siffror
+                .sum(); // plusar ihop alla varor och ger totala summan
+    }
+}
