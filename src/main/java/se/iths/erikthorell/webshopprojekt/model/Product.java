c2d9755b93a4173f98e2d0afc8c54bb6280972cf
OP-Devloop <oscar.pettersson@iths.se>
2026-04-14 10:32:16 +0200

    add Product entity with attributes and constructors

---
+++ src/main/java/se/iths/erikthorell/webshopprojekt/model/Product.java
@@ -0,0 +1,38 @@
+package se.iths.erikthorell.webshopprojekt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String category;
+
+    private double price;
+
+    private int stock;
+
+    private String description;
+
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(String name, String category, double price, int stock, String description, String image) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+        this.description = description;
+        this.image = image;
+    }
+}
