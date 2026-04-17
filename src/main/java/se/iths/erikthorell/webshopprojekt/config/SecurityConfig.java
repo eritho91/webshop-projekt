1c12bd1e3d31665e2069a7703f6520669408e8d1
eritho91 <erik.thorell@iths.se>
2026-04-17 12:59:23 +0200

    Login krävs bara för profile-sidorna

--- src/main/java/se/iths/erikthorell/webshopprojekt/config/SecurityConfig.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/config/SecurityConfig.java
@@ -3,39 +3,23 @@ package se.iths.erikthorell.webshopprojekt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authorization.EnableMultiFactorAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.FactorGrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import se.iths.erikthorell.webshopprojekt.service.MagicLinkOneTimeTokenGenerationSuccessHandler;
 
 @Configuration
-@EnableMultiFactorAuthentication(authorities = {
-        FactorGrantedAuthority.PASSWORD_AUTHORITY,
-        FactorGrantedAuthority.OTT_AUTHORITY
-})
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(
-            HttpSecurity http,
-            MagicLinkOneTimeTokenGenerationSuccessHandler magicLinkHandler
-    ) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/register", "/privacy-policy", "/cookie-policy").permitAll()
-                        .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
-                        .requestMatchers("/login/**", "/ott/**").permitAll()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                        .anyRequest().authenticated()
+                        .requestMatchers("/profile", "/profile/**").authenticated()
+                        .anyRequest().permitAll()
                 )
                 .formLogin(Customizer.withDefaults())
-                .oneTimeTokenLogin(ott -> ott
-                        .tokenGenerationSuccessHandler(magicLinkHandler)
-                )
                 .logout(logout -> logout
                         .logoutSuccessUrl("/")
                 );
