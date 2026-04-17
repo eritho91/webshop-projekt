13e28a604bf324f57ff4261b1f216aa36630bd13
eritho91 <erik.thorell@iths.se>
2026-04-17 14:44:01 +0200

    Det går att ta bort användare

--- src/main/java/se/iths/erikthorell/webshopprojekt/service/AppUserService.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/service/AppUserService.java
@@ -2,6 +2,7 @@ package se.iths.erikthorell.webshopprojekt.service;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import se.iths.erikthorell.webshopprojekt.model.AppUser;
 import se.iths.erikthorell.webshopprojekt.model.Role;
 import se.iths.erikthorell.webshopprojekt.repository.AppUserRepository;
@@ -26,5 +27,14 @@ public class AppUserService {
         user.setRole(Role.ROLE_USER);
 
         return appUserRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUserByEmail(String email) {
+        long deletedCount = appUserRepository.deleteByEmail(email);
+
+        if (deletedCount == 0) {
+            throw new IllegalArgumentException("Ingen användare hittades med den e-postadressen.");
+        }
     }
 }
