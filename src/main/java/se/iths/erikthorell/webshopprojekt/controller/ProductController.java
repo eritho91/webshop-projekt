d5aad6e340e69e9c3a3119897fb4c0c6ac0bf035
OP-Devloop <oscar.pettersson@iths.se>
2026-04-20 15:37:15 +0200

    add category repository and service, create product form, and update product controller

--- src/main/java/se/iths/erikthorell/webshopprojekt/controller/ProductController.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import se.iths.erikthorell.webshopprojekt.model.Category;
 import se.iths.erikthorell.webshopprojekt.model.Product;
+import se.iths.erikthorell.webshopprojekt.service.CategoryService;
 import se.iths.erikthorell.webshopprojekt.service.ProductService;
 
 import java.math.BigDecimal;
@@ -18,9 +19,12 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
+    private final CategoryService categoryService;
 
-    public ProductController(ProductService productService) {
+    public ProductController(ProductService productService,
+                             CategoryService categoryService) {
         this.productService = productService;
+        this.categoryService = categoryService;
     }
 
     @GetMapping
@@ -50,8 +54,21 @@ public class ProductController {
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
-    public Product createProduct(@Valid @RequestBody Product product) {
-        return productService.createProduct(product);
+    public String createProduct(@Valid @ModelAttribute Product product) {
+
+        Category category = categoryService.findById(product.getCategory().getId());
+        product.setCategory(category);
+
+        productService.createProduct(product);
+
+        return "redirect:/products";
+    }
+
+    @GetMapping("/new")
+    public String showCreateForm(Model model) {
+        model.addAttribute("product", new Product());
+        model.addAttribute("categories", categoryService.findAll());
+        return "create-product";
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
