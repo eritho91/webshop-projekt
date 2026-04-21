106ab1d2185ff830de94569d559fdf18efb29435
OP-Devloop <oscar.pettersson@iths.se>
2026-04-21 10:39:26 +0200

    remove unused methods from ProductRepository to streamline code

--- src/main/java/se/iths/erikthorell/webshopprojekt/repository/ProductRepository.java
+++ src/main/java/se/iths/erikthorell/webshopprojekt/repository/ProductRepository.java
@@ -1,23 +1,20 @@
 package se.iths.erikthorell.webshopprojekt.repository;
 
-import jakarta.validation.constraints.Positive;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import se.iths.erikthorell.webshopprojekt.model.Category;
 import se.iths.erikthorell.webshopprojekt.model.Product;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findByName(String name);
-
-    Product findByCategory(Category category);
-
-    Product findByPrice(@Positive BigDecimal price);
+//    Product findByName(String name);
+//
+//    Product findByCategory(Category category);
+//
+//    Product findByPrice(@Positive BigDecimal price);
 
     Optional<Product> findById(Long id);
 
